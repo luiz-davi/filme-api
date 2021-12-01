9f7b2e525303c203e72439272bf21b5abf1c217c
Luiz Davi <55983920+luiz-davi@users.noreply.github.com>
2021-11-30 23:11:51 -0300

    Povoando banco

--- app/models/filme.rb
+++ app/models/filme.rb
@@ -1,2 +1,10 @@
 class Filme < ApplicationRecord
+
+    # validates :show_id, presence: true
+    # validates :tipo, presence: true
+    # validates :titulo, presence: true
+    # validates :diretor, presence: true
+    # validates :elenco, presence: true
+    # validates :duracao, presence: true
+    # validates :categoria, presence: true
 end
